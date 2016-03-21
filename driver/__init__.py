22e0933ad4e6836a01fc3f5cbe054bd0c050b507
sschwarcz <schwarcz@cs.umd.edu>
2016-03-21 15:42:29 -0400

    Added GUI back into driver

--- driver/__init__.py
+++ driver/__init__.py
@@ -5,8 +5,6 @@ from ai.greedy_ai.cheapest_path_ai import CheapestPathAI
 from ai.greedy_ai.greedy_ai import GreedyAI
 from ai.random_ai import RandomAI
 from game import Game
-from game.methods import find_paths_for_destinations
-from game.board import create_board
 
 p1 = CheapestPathAI("P1")
 # p2 = Player("P2")
@@ -14,7 +12,7 @@ p2 = GreedyAI("P2")
 
 players = [p1, p2]
 
-# game_gui = gui.GUI()
+game_gui = gui.GUI()
 game = Game(players)
 
 # Main game loop.  Tells players when to take their turn.
@@ -22,7 +20,7 @@ while not game.is_game_over()[0]:
     for player in players:
         if game.is_turn(player):
             player.take_turn(game)
-            # game_gui.update(game)
+            game_gui.update(game)
             break
 
 # Game's over.  Tell the players and print out some results.
@@ -32,4 +30,4 @@ for player in players:
 print "Game Over"
 print "Winner: %s" % game.is_game_over()[1]
 print "Final Scores: %s" % game.get_visible_scores()
-# game_gui.update_game_ended(game)
+game_gui.update_game_ended(game)
