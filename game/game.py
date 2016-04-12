35340954d04620b1a3ec8f14284dc268856721ac
Jeff Twigg <jeffrey.n.twigg.ctr@mail.mil>
2016-04-12 13:16:16 -0400

    Modified game.game.py again  maybe this will clear things up. If there are issues let's talk

--- game/game.py
+++ game/game.py
@@ -616,51 +616,14 @@ class Game:
         if edge.cost > num_cars:
             return result
 
-        ###### This below is Jeff's Version########
-        ## print 'checking stuff'
-        ## Route has no color.
-        # if edge.color == Colors.none:
-        #     for card in cards:
-        #         if card != Colors.none and cards[card] + cards[Colors.none] >= edge.cost:
-        #             # Find all possible combinations of cards that can be used to claim the edge.
-        #             # Using min(edge.cost - 1) guarantees that we will not accidentally add unnecessary plays that
-        #             # use all wilds.
-        #             for i in range(edge.cost - 1, cards[Colors.none] + 1):
-        #                 if cards[card] >= edge.cost - i:
-        #                     #print 'adding card %f: ' % cards[card]
-        #                     #print  card
-        #                     #print 'using %f wild cards' % i
-        #                     #print 'for %f car length edge' %edge.cost
-        #                     result.append(ConnectAction(edge, Counter({card: edge.cost - i, Colors.none: i})))
-        #
-        # # Route has a color.
-        # else:
-        #     if cards[edge.color] + cards[Colors.none] >= edge.cost:
-        #         # Find all possible combinations of cards that can be used to claim the edge.
-        #         # Using min(edge.cost - 1) guarantees that we will not accidentally add unnecessary plays that
-        #         # use all wilds.
-        #         for i in range(0,cards[Colors.none] + 1 ):
-        #             if cards[edge.color] >= edge.cost - i:
-        #                 #print 'adding card %f: ' % cards[edge.color]
-        #                 #print  edge.color
-        #                 #print 'using %f wild cards' % i
-        #                 #print 'for %f car length edge' % edge.cost
-        #                 result.append(ConnectAction(edge, Counter({edge.color: edge.cost - i, Colors.none: i})))
-        #
-        # # If player has enough wilds to just get the route on wilds.
-        # if cards[Colors.none] >= edge.cost:
-        #     result.append(ConnectAction(edge, Counter({Colors.none: edge.cost})))
-        # return result
-        ####################
-
-        ### This below is Steve's Version
+        # Route has no color.
         if edge.color == Colors.none:
             for card in cards:
                 if card != Colors.none and cards[card] + cards[Colors.none] >= edge.cost:
                     # Find all possible combinations of cards that can be used to claim the edge.
                     # Using min(edge.cost - 1) guarantees that we will not accidentally add unnecessary plays that
                     # use all wilds.
-                    for i in range(min(edge.cost - 1, cards[Colors.none] + 1)):
+                    for i in range(0, min(cards[Colors.none] + 1,edge.cost)):
                         if cards[card] >= edge.cost - i:
                             result.append(ConnectAction(edge, Counter({card: edge.cost - i, Colors.none: i})))
         # Route has a color.
@@ -669,7 +632,7 @@ class Game:
                 # Find all possible combinations of cards that can be used to claim the edge.
                 # Using min(edge.cost - 1) guarantees that we will not accidentally add unnecessary plays that
                 # use all wilds.
-                for i in range(min(edge.cost - 1, cards[Colors.none] + 1)):
+                for i in range(0,cards[Colors.none] + 1 ):
                     if cards[edge.color] >= edge.cost - i:
                         result.append(ConnectAction(edge, Counter({edge.color: edge.cost - i, Colors.none: i})))
 
@@ -678,8 +641,7 @@ class Game:
             result.append(ConnectAction(edge, Counter({Colors.none: edge.cost})))
 
         return result
-        #################
-
+ 
 
     def _lose_cards(self, player, cards):
         """
